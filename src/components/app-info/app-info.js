57c61b58ea3cbb6d24ef7712b315e654cd5be8ae
Dmytro <lypovetskyidmytro@gmail.com>
2023-03-10 14:28:28 +0300

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в Компании N</h1>
+            <h2>Общее число сотрудников: </h2>
+            <p2>Премию получат: </p2>
+        </div>
+    )
+}
+
+export default AppInfo;
