837ce8f15854efa6d8c1d2fedd40716c68d11712
Dmytro <lypovetskyidmytro@gmail.com>
2023-03-12 20:08:03 +0300

    first commit

--- src/components/employees-list-item/emploees-list-item.js
+++ src/components/employees-list-item/emploees-list-item.js
@@ -1,8 +1,6 @@
 import './employees-list-item.css';
 
-
 const EmployeesListItem = (props) => {
-
     const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
     let classNames = "list-group-item d-flex justify-content-between";
